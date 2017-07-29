f23b909c5a140afd4eaa12bd185b6f5591835276
sfsheng0322 <sfsheng0322@gmail.com>
2017-07-29 20:20:33 +0800

    init project

---
+++ app/src/main/java/com/sunfusheng/scrollable/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sunfusheng.scrollable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
